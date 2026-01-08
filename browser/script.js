1016745103d050cd3ef4d1693e5620bed2b2dcb9
catatan-pemain <sportsbook.co.id@gmail.com>
2026-01-08 20:31:24 +0700

    Update script.js

--- browser/script.js
+++ browser/script.js
@@ -34,3 +34,29 @@ goButton.addEventListener('click', redirectByKeyword);
 urlInput.addEventListener('keydown', e => {
     if (e.key === 'Enter') redirectByKeyword();
 });
+
+fetch('data.json')
+  .then(res => res.json())
+  .then(data => renderFeed(data));
+
+function renderFeed(items) {
+    const feed = document.getElementById('feed');
+
+    items.forEach(item => {
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        card.innerHTML = `
+            <img src="${item.image}">
+            <h3>${item.title}</h3>
+            <p>${item.desc}</p>
+        `;
+
+        card.onclick = () => {
+            localStorage.setItem('lastTag', item.tag);
+            window.location.href = item.link;
+        };
+
+        feed.appendChild(card);
+    });
+}
