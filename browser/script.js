c3da338752c66bd8487d9280e020c138530109ee
catatan-pemain <sportsbook.co.id@gmail.com>
2026-01-08 21:10:38 +0700

    Update script.js

--- browser/script.js
+++ browser/script.js
@@ -35,36 +35,3 @@ urlInput.addEventListener('keydown', e => {
     if (e.key === 'Enter') redirectByKeyword();
 });
 
-fetch('data.json')
-  .then(res => res.json())
-  .then(data => renderFeed(data));
-
-function renderFeed(items) {
-    const feed = document.getElementById('feed');
-
-    items.forEach(item => {
-        const card = document.createElement('div');
-        card.className = 'card';
-
-        card.innerHTML = `
-            <img src="${item.image}">
-            <h3>${item.title}</h3>
-            <p>${item.desc}</p>
-        `;
-
-        card.onclick = () => {
-            localStorage.setItem('lastTag', item.tag);
-            window.location.href = item.link;
-        };
-
-        feed.appendChild(card);
-    });
-}
-
-const lastTag = localStorage.getItem('lastTag');
-
-if (lastTag) {
-    items.sort((a, b) =>
-        a.tag === lastTag ? -1 : 1
-    );
-}
