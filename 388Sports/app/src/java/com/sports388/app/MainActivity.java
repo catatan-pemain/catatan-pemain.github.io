e2f54a6ec6597b28fc02234ca921426b2b99cf69
catatan-pemain <sportsbook.co.id@gmail.com>
2026-01-11 11:56:55 +0700

    Add files via upload

---
+++ 388Sports/app/src/java/com/sports388/app/MainActivity.java
@@ -0,0 +1,41 @@
+package com.sports388.app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUserAgentString(
+                settings.getUserAgentString() + " 388SportsApp"
+        );
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://388sports.beauty/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
